ecc000626b780dec21b5fe3b0a993e6d9bf64943
Kerim Yarmakov <kyarmakov@gmail.com>
2023-02-02 22:55:52 +0300

    create stock-inventory module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StockInventoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
