e0d6526735b44bf7b23248a83df00d3ee8f6892c
Kerim Yarmakov <kyarmakov@gmail.com>
2023-02-02 23:48:49 +0300

    create form and register formGroup item

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-inventory',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./stock-inventory.component.scss']
 })
 export class StockInventoryComponent {
+  form: FormGroup = this.fb.group({
+    item: this.fb.group({
+      name: [''],
+      price: ['']
+    }),
+    
+  });
 
+
+  constructor(private fb: FormBuilder) {}
 }
--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 
@@ -9,7 +10,8 @@ import { StockInventoryComponent } from './containers/stock-inventory/stock-inve
     StockInventoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     StockInventoryComponent
